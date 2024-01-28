44d69e727ad81baf9115c07bd2e946448c16eae4
Vincent Revard <Vincent.Revard@gmail.com>
2024-01-27 21:59:43 -0800

    Lab Complete

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let freeRide, chargeTwenty, chargeThirty, chargeNever;
+  if (distance <= 400) 
+    return freeRide = 'This one is on me!'
+  else if (distance >= 400)
+  if (distance <= 2000)
+    return chargeTwenty = 'That will be twenty bucks.'
+  else if (distance > 2000)
+  if (distance < 2500)
+    return chargeThirty = 'I will gladly take your thirty bucks.'
+  else (distance >= 2500)
+  return chargeNever = 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+ return (city === 'NYC')? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous' :
+      return 'Thank you so much.' 
+    case 'not as generous' : 
+      return 'Thank you.';
+    default : 
+      return 'Bye.';      
+  }
 }
